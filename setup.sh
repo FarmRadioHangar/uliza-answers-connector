220638c2bb51045177f198f13c98c43ed5fd881b
Johannes Hilden <hildenjohannes@gmail.com>
2018-03-07 13:02:26 +0300

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p 'Enter your Viamo API key: ' VIAMO_API_KEY && \
 read -p 'Enter the Zammad API token: ' ZAMMAD_API_TOKEN && \
-printf "ZAMMAD_API_TOKEN=$ZAMMAD_API_TOKEN\\nVIAMO_API_KEY=$VIAMO_API_KEY" > .env
+printf "ZAMMAD_API_TOKEN=$ZAMMAD_API_TOKEN\nVIAMO_API_KEY=$VIAMO_API_KEY\n" > .env
 read -r -p "Run npm install? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]] 
 then 
