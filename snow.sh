dab0fedf9a3a3602f50b87c788c6723e84895f36
Johannes Hilden <hildenjohannes@gmail.com>
2018-03-07 12:44:43 +0300

    Create Bart Snow campaign test script

---
+++ snow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p 'Phone number to call: ' PHONE_NUMBER && \
+read -p 'Webkhook URL (e.g., "http://xxxxxxx.ngrok.io"): ' WEBHOOK_URL && \
+curl \
+  -XPOST \
+  https://go.votomobile.org/api/v1/outgoing_calls/ \
+  -H 'Content-Type: application/json' \
+  -d '{"send_to_phones":"$PHONE_NUMBER","tree_id":"22881","api_key":"$VIAMO_API_KEY","webhook_url":"$WEBHOOK_URL/update?audio_block_id=9761370","webhook_method":"POST"}'
