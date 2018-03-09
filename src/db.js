a378066600e247b3adccc6cf0417abd5ede5cf47
Johannes Hilden <hildenjohannes@gmail.com>
2018-03-09 12:24:00 +0300

    Add db code for creating ticket

--- src/db.js
+++ src/db.js
@@ -1,5 +1,21 @@
 var sqlite = require('sqlite');
 
+var db;
+
 module.exports = {
+
+  init: function() {
+    return sqlite.open('db.sqlite')
+    .then(function(connection) {
+      db = connection;
+      return db.run('CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY, zammad_id INTEGER, subscriber_phone TEXT, audio TEXT, created_at TEXT);')
+    });
+  },
+
+  createTicket: function(zammadId, subscriber, audio) {
+    return db.run(
+      'INSERT INTO tickets (zammad_id, subscriber_phone, audio, created_at) VALUES (?, ?, ?, DATETIME(\'now\'));', zammadId, subscriber, audio
+    );
+  }
 
 };
