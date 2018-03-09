74ada50b99579b7368c5ed33a14325985dcc0459
Johannes Hilden <hildenjohannes@gmail.com>
2018-03-09 13:09:54 +0300

    Add methods for listing tickets and updating article count

--- src/db.js
+++ src/db.js
@@ -8,14 +8,24 @@ module.exports = {
     return sqlite.open('db.sqlite')
     .then(function(connection) {
       db = connection;
-      return db.run('CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY, zammad_id INTEGER, subscriber_phone TEXT, audio TEXT, created_at TEXT);')
+      return db.run('CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY, zammad_id INTEGER, subscriber_phone TEXT, audio TEXT, articles INTEGER, created_at TEXT);')
     });
   },
 
   createTicket: function(zammadId, subscriber, audio) {
     return db.run(
-      'INSERT INTO tickets (zammad_id, subscriber_phone, audio, created_at) VALUES (?, ?, ?, DATETIME(\'now\'));', zammadId, subscriber, audio
+      'INSERT INTO tickets (zammad_id, subscriber_phone, audio, articles, created_at) VALUES (?, ?, ?, 1, DATETIME(\'now\'));', zammadId, subscriber, audio
     );
+  },
+
+  updateArticlesCount(id, count) {
+    return db.run(
+      'UPDATE tickets SET articles = ? WHERE id = ?;', count, id
+    );
+  },
+
+  getTickets: function() {
+    return db.all('SELECT * FROM tickets');
   }
 
 };
