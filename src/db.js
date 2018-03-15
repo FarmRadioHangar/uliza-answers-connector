8460c8a6aa84fd644cd74f1268e10e1f0bf61aca
Johannes Hilden <hildenjohannes@gmail.com>
2018-03-16 02:08:20 +0300

    Add users table and database api

--- src/db.js
+++ src/db.js
@@ -9,6 +9,9 @@ module.exports = {
     .then(function(connection) {
       db = connection;
       return db.run('CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY, zammad_id INTEGER, subscriber_phone TEXT, audio TEXT, articles INTEGER, created_at TEXT);')
+    })
+    .then(function() {
+      return db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, auth0_user_id INTEGER, zammad_token TEXT, firebase_login TEXT, sip_user TEXT, created_at TEXT);')
     });
   },
 
@@ -26,6 +29,12 @@ module.exports = {
 
   getTickets: function() {
     return db.all('SELECT * FROM tickets;');
+  },
+
+  createUser: function(auth0Id, zammadToken, firebaseLogin, sipUser) {
+    return db.run(
+      'INSERT INTO users (auth0_user_id, zammad_token, firebase_login, sip_user, created_at) VALUES (?, ?, ?, ?, DATETIME(\'now\'));', auth0Id, zammadToken, firebaseLogin, sipUser
+    );
   }
 
 };
