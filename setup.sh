f657a12beae75259ac558d313bbd9e33b16224ed
Johannes Hilden <hildenjohannes@gmail.com>
2018-03-15 21:01:08 +0300

    Remove unnecessary line breaks in bash script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-read -p 'Enter your Viamo API key: ' VIAMO_API_KEY && \
-read -p 'Enter the Zammad API token: ' ZAMMAD_API_TOKEN && \
+read -p 'Enter your Viamo API key: ' VIAMO_API_KEY
+read -p 'Enter the Zammad API token: ' ZAMMAD_API_TOKEN
 printf "ZAMMAD_API_TOKEN=$ZAMMAD_API_TOKEN\nVIAMO_API_KEY=$VIAMO_API_KEY\n" > .env
 read -r -p "Run npm install? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]] 
