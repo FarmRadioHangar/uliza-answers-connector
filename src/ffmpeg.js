242a31967feb2f6974e6deb7057b2696137c6ec1
Johannes Hilden <hildenjohannes@gmail.com>
2018-04-06 14:36:22 +0300

    Create separate module for ffmpeg stuff

---
+++ src/ffmpeg.js
@@ -0,0 +1,28 @@
+var base64 = require('base64-stream');
+var ffmpeg = require('fluent-ffmpeg');
+
+module.exports = {
+
+  encodeAudio: function(url) {
+    return new Promise(function(resolve, reject) {
+      var output = new base64.Encode();
+      ffmpeg().input(request.get({
+        url: url,
+        encoding: null,
+      }))
+      .outputFormat('mp3')
+      .pipe(output);
+      var buffer = '';
+      output.on('data', function(chunk) {
+        buffer += chunk.toString();
+      });
+      output.on('end', function() {
+        resolve(buffer);
+      });
+      output.on('error', function(error) {
+        reject(error);
+      });
+    });
+  }
+
+};
