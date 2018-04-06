db2fecd119cf656c1bd68330343ebc2b3a005dfa
Johannes Hilden <hildenjohannes@gmail.com>
2018-04-06 15:05:42 +0300

    Create getTicketAttachment method

--- src/zammad.js
+++ src/zammad.js
@@ -1,8 +1,8 @@
 var requestJson = require('request-json');
-var api = require('./api');
+var api         = require('./api');
 
 var ZAMMAD_API_TOKEN = process.env.ZAMMAD_API_TOKEN;
-var ZAMMAD_API_URL = process.env.ZAMMAD_API_URL || 
+var ZAMMAD_API_URL = process.env.ZAMMAD_API_URL ||
   'https://answers.uliza.fm/api/v1/';
 
 var zammad = requestJson.createClient(ZAMMAD_API_URL);
@@ -25,6 +25,16 @@ module.exports = {
 
   patch: function(uri, data, options) {
     return api.makeRequest(zammad, uri, options, 'PATCH', data);
+  },
+
+  getTicketAttachment: function(ticketId, articleId, attachmentId) {
+    return request.get({
+      url: ZAMMAD_API_URL + 'ticket_attachment/' + ticketId
+                                           + '/' + articleid
+                                           + '/' + attachmentId,
+      encoding: null,
+      headers: {Authorization: 'Token token=' + ZAMMAD_API_TOKEN}
+    });
   }
 
 };
