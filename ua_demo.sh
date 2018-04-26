e6e622989302eaa6ede88aca73149e4dd423d4fa
Johannes Hilden <hildenjohannes@gmail.com>
2018-04-26 21:08:55 +0300

    Update block id for new demo

--- ua_demo.sh
+++ ua_demo.sh
@@ -2,7 +2,7 @@
 read -p 'Phone number to call: ' PHONE_NUMBER && \
 read -p 'Webkhook URL (e.g., "http://xxxxxxx.ngrok.io"): ' WEBHOOK_URL && \
 eval $(cat .env | sed 's/^/export /')
-PAYLOAD="{\"send_to_phones\":\"$PHONE_NUMBER\",\"tree_id\":\"25106\",\"api_key\":\"$VIAMO_API_KEY\",\"webhook_url\":\"$WEBHOOK_URL/update?audio_block_id=9761370\",\"webhook_method\":\"POST\"}"
+PAYLOAD="{\"send_to_phones\":\"$PHONE_NUMBER\",\"tree_id\":\"25106\",\"api_key\":\"$VIAMO_API_KEY\",\"webhook_url\":\"$WEBHOOK_URL/update?audio_block_id=10622540\",\"webhook_method\":\"POST\"}"
 curl \
   -XPOST \
   https://go.votomobile.org/api/v1/outgoing_calls/ \
