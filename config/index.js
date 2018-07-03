bd495cab8a5a57d297490bc80b2bce1ef32289f1
Johannes Hilden <hildenjohannes@gmail.com>
2018-07-03 11:58:51 +0300

    Implement wizard

--- config/index.js
+++ config/index.js
@@ -1,20 +1,7 @@
+var fs      = require('fs');
 var ora     = require('ora');
 var request = require('request-promise');
 var term    = require('./terminal-kit-promise');
-
-/*
-
- 1. User enters API key.
-
-    - Validate key
-    - Print information
-
- 2. User chooses language.
- 3. User chooses tree.
- 4. User chooses tree block used for open-ended question.
- 5.
-
- */
 
 term.$.grabInput();
 
@@ -24,7 +11,7 @@ term.$.on('key', key => {
   }
 });
 
-let key, spinner, languages, language;
+let key, spinner, languages, language, trees, tree, blocks, block, config;
 
 function viamoRequest(endpoint) {
   return request(`https://go.votomobile.org/api/v1/${endpoint}`, {
@@ -34,10 +21,17 @@ function viamoRequest(endpoint) {
 
 return Promise.resolve()
   .then(() => term.$.clear())
-  .then(() => term.term('Enter a Viamo API key to use for this campaign: '))
+  .then(() => term.drawImage('./logo.png', { shrink: { width: 40, height: 40 } }))
+  .then(() => {
+    term.$.bold('\nWelcome to the Uliza Answers configuration wizard.');
+    term.$.wrap('\nThis will take you through the process of configuring Uliza Answers for your campaign. To complete this wizard, you need the following prerequisites:');
+    term.term('\n\n- A Viamo API key. This can be obtained from https://go.votomobile.org/settings (under the \'API Key\' tab).\n');
+    term.term('- A tree which includes the open-ended question block that asks the participant to contribute their question.\n');
+    term.term('\nEnter the Viamo API key: ');
+  })
   .then(() => term.inputField())
   .then(input => {
-    term.term('\n');
+    term.$.clear();
     spinner = ora('Verifying API key');
     spinner.start();
     key = input;
@@ -52,29 +46,81 @@ return Promise.resolve()
     }
     process.exit(1);
   })
-  .then(response => {
+  .then(() => {
     spinner.succeed('OK');
+    return viamoRequest('languages');
   })
-  .then(() => viamoRequest('languages'))
   .then(response => {
-    term.$.clear();
-    term.term('Select language:\n================\n');
+    term.term('\nSelect the language of this campaign:\n=====================================\n');
+    term.term('\nYou can add new languages from https://go.votomobile.org/languages/new.\n');
     languages = JSON.parse(response).data.languages;
     return term.singleColumnMenu(
       languages.map(lang => lang.name)
     );
   })
   .then(response => {
+    term.$.clear();
     language = languages[response.selectedIndex];
+    spinner = ora();
+    spinner.start();
     return viamoRequest('trees');
   })
   .then(response => {
+    spinner.stop();
     term.$.clear();
-    term.term('Select a tree:\n==============\n');
+    term.term('Select the campaign tree:\n=========================\n');
     trees = JSON.parse(response).data.trees;
     return term.singleColumnMenu(
       trees.map(tree => `${tree.id}\t${tree.title}`)
     );
+  })
+  .then(response => {
+    term.$.clear();
+    tree = trees[response.selectedIndex];
+    spinner = ora();
+    spinner.start();
+    return viamoRequest(`trees/${tree.id}/blocks`);
+  })
+  .then(response => {
+    spinner.stop();
+    term.$.clear();
+    term.term('Select the user question tree block:\n====================================\n');
+    term.term('\nThis is the tree block that prompts the participant to ask their question to the system.\n');
+    blocks = JSON.parse(response).data.blocks.filter(
+      block => 'Open-Ended Question' === block.type
+    );
+    if (!blocks.length) {
+      term.$.brightRed('\nNo open ended question blocks were found in this tree.\n');
+      process.exit();
+    }
+    return term.singleColumnMenu(
+      blocks.map(block => `${block.id} ${block.details.title}`)
+    );
+  })
+  .then(response => {
+    term.$.clear();
+    block = blocks[response.selectedIndex];
+  })
+  .then(() => {
+    config  = `VIAMO_API_KEY=${key}\n`;
+    config += `VIAMO_TREE_ID=${tree.id}\n`;
+    config += `VIAMO_BLOCK_ID=${block.id}\n`;
+    config += `LANGUAGE_ID=${language.id}\n`;
+    term.term(`${config}\n`);
+    term.term('Write configuration to .env? [Y|n] ');
+    return term.yesOrNo({
+      yes: ['y', 'ENTER'],
+      no: ['n']
+    });
+  })
+  .then(result => {
+    term.term('\n\n');
+    if (result) {
+      fs.writeFileSync('.env', config);
+      term.term('✔ Configuration saved.\n');
+    } else {
+      term.$.brightRed('Configuration not saved.\n');
+    }
   })
   .then(() => {
     process.exit();
